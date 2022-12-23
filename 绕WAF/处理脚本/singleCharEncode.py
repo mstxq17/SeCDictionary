605657b157de560f5149cc64b85f2fa5cb90e0a7
mstxq17 <154380808@qq.com>
2022-12-23 10:10:29 +0800

    Adjust file place

--- 绕WAF/处理脚本/singleCharEncode.py
+++ 绕WAF/处理脚本/singleCharEncode.py
@@ -5,7 +5,7 @@
 生成0-255单字符的url编码，用来绕waf测试
 """
 
-with open("singleCharEncode.txt", "w") as f:
+with open("../singleCharEncode.txt", "w") as f:
     for i in range(0, 256):
         ok_str = "%" + str(hex(i)).split("0x")[1].zfill(2)
         f.write(ok_str + "\n")

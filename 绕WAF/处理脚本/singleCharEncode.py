41f2067a17a98edc330f4a013c1598ce2756c610
mstxq17 <154380808@qq.com>
2022-12-23 10:08:53 +0800

    Add Folder about bypass waf

---
+++ 绕WAF/处理脚本/singleCharEncode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+生成0-255单字符的url编码，用来绕waf测试
+"""
+
+with open("singleCharEncode.txt", "w") as f:
+    for i in range(0, 256):
+        ok_str = "%" + str(hex(i)).split("0x")[1].zfill(2)
+        f.write(ok_str + "\n")
